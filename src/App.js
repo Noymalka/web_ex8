aaf60877b874a2bc2dc809d7e34bc12895abecdc
Noymalka <Noy.malka@e.braude.ac.il>
2024-03-10 14:18:29 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [images, setImages] = useState([]);
+  const [selectedImageIndex, setSelectedImageIndex] = useState(0);
+
+  // Similar to ng-repeat in AngularJS
+  const renderImages = () => {
+    return images.map((image, index) => (
+      <img
+        key={index}
+        src={image.smallimgUrl}
+        alt="Smallimg"
+        className="smallimg"
+        onClick={() => selectImage(index)}
+      />
+    ));
+  };
+
+  const selectImage = (index) => {
+    setSelectedImageIndex(index);
+  };
+
+  const nextImage = () => {
+    setSelectedImageIndex((selectedImageIndex + 1) % images.length);
+  };
+
+  const prevImage = () => {
+    setSelectedImageIndex((selectedImageIndex - 1 + images.length) % images.length);
+  };
+
+  // Fetch images when component mounts
+  React.useEffect(() => {
+    const fetchImages = async () => {
+      const fetchedImages = [];
+      for (let i = 1; i <= 10; i++) {
+        fetchedImages.push({
+          id: i,
+          url: `https://picsum.photos/id/${i}/350/350`,
+          smallimgUrl: `https://picsum.photos/id/${i}/50/50`,
+        });
+      }
+      setImages(fetchedImages);
+    };
+    fetchImages();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="image-container">
+      <div className="smallimg-container">{renderImages()}</div>
+      <div className="main-img-container">
+        <img src={images[selectedImageIndex]?.url} alt="Selected" className="main-img" />
+      </div>
+      <div className="arrows">
+        <span className="arrow" onClick={prevImage}>
+          &larr; 
+        </span>
+        <span className="arrow" onClick={nextImage}>
+          &rarr;
+        </span>
+      </div>
     </div>
   );
 }
